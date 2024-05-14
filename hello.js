94d92273e8a655d7445d2d3c99e632d490ca9a00
Kapil Rai <kapil.raii19971@gmail.com>
2024-05-14 07:35:10 +0545

    Hello World!
    Printing Hello world in  web with js

---
+++ hello.js
@@ -0,0 +1,8 @@
+// printing hello world with console.log
+console.log("Hello World!");
+
+// print hello world with alert
+alert("Hello World!");
+
+// Printing Hello World with document.write
+document.write("Hello World!")
